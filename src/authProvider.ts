0c12b41962fc11f1c8bea6db320b29f757b3de3e
Sait Yaşar <98064449+saitYasar@users.noreply.github.com>
2025-01-18 14:00:18 +0300

    Kullanıcı oturum açma işlemi için profil resmi kaydedildi; uygulama çubuğuna kullanıcı resmi eklendi.

--- src/authProvider.ts
+++ src/authProvider.ts
@@ -1,32 +1,32 @@
-import { AUTH_LOGIN, AUTH_LOGOUT, AUTH_CHECK } from 'react-admin';
+import { AUTH_LOGIN, AUTH_LOGOUT, AUTH_CHECK } from "react-admin";
 
 export default (type: any, params: any) => {
   console.log("type", type);
-  
-    if (type === AUTH_LOGIN) {
-        const { username, password } = params;
-        const request = new Request('https://api.mznekip.com/auth/login', {
-            method: 'POST',
-            body: JSON.stringify({ username, password }),
-            headers: new Headers({ 'Content-Type': 'application/json' }),
-        })
-        return fetch(request)
-            .then(response => {
-                if (response.status < 200 || response.status >= 300) {
-                    throw new Error(response.statusText);
-                }
-                return response.json();
-            })
-          .then(({ token, id }) => {
-              localStorage.setItem('token', token);
-            localStorage.setItem('id', id);
-            });
-        
-    } else if (type === AUTH_LOGOUT) {
-        localStorage.removeItem('token');
-        return Promise.resolve();
-    } else if (type === AUTH_CHECK) {
-        return localStorage.getItem('token') ? Promise.resolve() : Promise.reject();
-    }
+
+  if (type === AUTH_LOGIN) {
+    const { username, password } = params;
+    const request = new Request("https://api.mznekip.com/auth/login", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: new Headers({ "Content-Type": "application/json" }),
+    });
+    return fetch(request)
+      .then((response) => {
+        if (response.status < 200 || response.status >= 300) {
+          throw new Error(response.statusText);
+        }
+        return response.json();
+      })
+      .then(({ token, id, image }) => {
+        localStorage.setItem("token", token);
+        localStorage.setItem("id", id);
+        localStorage.setItem("image", image);
+      });
+  } else if (type === AUTH_LOGOUT) {
+    localStorage.removeItem("token");
     return Promise.resolve();
-}
+  } else if (type === AUTH_CHECK) {
+    return localStorage.getItem("token") ? Promise.resolve() : Promise.reject();
+  }
+  return Promise.resolve();
+};
--- src/components/ra-ui/appbar/index.tsx
+++ src/components/ra-ui/appbar/index.tsx
@@ -1,23 +1,41 @@
-import { AppBar, TitlePortal } from 'react-admin';
-import SettingsIcon from '@mui/icons-material/Settings';
-import { IconButton } from '@mui/material';
-import DistedavimLogo from '../logo';
+import { AppBar, TitlePortal } from "react-admin";
+import SettingsIcon from "@mui/icons-material/Settings";
+import { IconButton } from "@mui/material";
+import DistedavimLogo from "../logo";
 
 const SettingsButton = () => (
-    <IconButton color="inherit">
-        <SettingsIcon />
-    </IconButton>
+  <IconButton color="inherit">
+    <SettingsIcon />
+  </IconButton>
 );
 
-export const MyAppBar = () => (
-    <AppBar style={{
-        background: 'linear-gradient(to right,rgb(157, 163, 74),rgb(254, 247, 37))',
-        animation: 'gradientAnimation 1s infinite'
-    }}>
-        <TitlePortal />
-        <div style={{ marginTop: "5px" }}>
-            <DistedavimLogo colors="white" width={120} height={40} />
-        </div>
-        <SettingsButton />
+export const MyAppBar = () => {
+  const data = localStorage.getItem("image");
+  return (
+    <AppBar
+      style={{
+        background:
+          "linear-gradient(to right,rgb(157, 163, 74),rgb(254, 247, 37))",
+        animation: "gradientAnimation 1s infinite",
+      }}
+    >
+      <TitlePortal />
+      <div style={{ marginTop: "5px" }}>
+        <DistedavimLogo colors="white" width={120} height={40} />
+      </div>
+      {data && (
+        <img
+          style={{
+            width: "40px",
+            height: "40px",
+            marginLeft: "20px",
+            borderRadius: "50%",
+          }}
+          src={data}
+          alt=""
+        />
+      )}
+      <SettingsButton />
     </AppBar>
-);
+  );
+};
